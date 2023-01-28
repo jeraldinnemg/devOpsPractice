f7e6d17969860dc384885fe380a6b33b5f0fb555
Jeraldinne Molleda <jeraldinne.molleda@gds.ey.com>
2023-01-28 16:47:10 -0300

    cicd

--- scripts/createResource.ps1
+++ scripts/createResource.ps1
@@ -2,9 +2,9 @@
 #-------- Parameters for the script  --------- 
 #------------------------------------------------------- 
 param(
-    [ValidateSet("create", "delete")]
-    [Parameter(Mandatory)][string]$Action,
     [Parameter(Mandatory)][string]$ResourceName,
+    $locationPrimary = "East US",
+    $locationSecondary = "West US",
     [switch]$ResourceGroup,
     [switch]$AppServicePlan,
     [switch]$AppService,
@@ -20,12 +20,6 @@ param(
 #-----------Function to create all resoruces ----------
 #-------------------------------------------------------
 
-
-function CreateAllResources {
-  param(
-  $locationPrimary = "East US",
-  $locationSecondary = "West US"
-  )
 
 
   $resourceGName = "USEDCTPJMRSG01"
@@ -144,144 +138,144 @@ function CreateAllResources {
 #    Write-LogCustom -Message "Failed to create App Service Plan $AppInsightsName"
 #  }
 
-}
-
-function CreateResourceGroup {
-  param(
-    [Parameter(Mandatory)][string]$ResourceGroupName,
-    $location = "eastus"
-  )
-  if (!(ValidateResourceExists -RsgOrRsc "rsg" -ResourceName $ResourceGroupName)) {
-    New-AzResourceGroup -Name $ResourceGroupName -Location $location
-  }
-}
-
-function CreateAppServicePlan {
-  param(
-    [Parameter(Mandatory)][string]$ResourceGroupName,
-    [Parameter(Mandatory)][string]$AppServicePlanName,
-    $location = "East US"
-  )
-  # crear asp
-  New-AzAppServicePlan -ResourceGroupName $ResourceGroupName -Name $AppServicePlanName -Location $location -Tier "F1"
-  #validar que se haya creado
-  if (ValidateResourceExists -RsgOrRsc "rsc" -ResourceName $AppServicePlanName) {
-    Write-LogCustom -Message "App Service Plan $AppServicePlanName created successfully"
-  }
-  else {
-    Write-LogCustom -Message "Failed to create App Service Plan $AppServicePlanName"
-  }
-}
-
-function CreateAppService {
-  param(
-    [Parameter(Mandatory)][string]$ResourceGroupName,
-    [Parameter(Mandatory)][string]$AppServicePlanName,
-    [Parameter(Mandatory)][string]$AppServiceName,
-    $location = "West US"
-  )
-  # create app service WAP
-
-  New-AzWebApp  `
-    -Name $AppServiceName `
-    -ResourceGroupName $ResourceGroupName `
-    -AppServicePlan $AppServicePlanName   `
-    -Location $location `
-
-  #Validate if resource exist
-  if (ValidateResourceExists -RsgOrRsc "rsc" -ResourceName $AppServiceName) {
-    Write-LogCustom -Message "App Service $AppServiceName created successfully"
-  }
-  else {
-    Write-LogCustom -Message "Failed to create App Service Plan $AppServiceName"
-  }
-}
-
-function CreateAppInsights {
-  param(
-    [Parameter(Mandatory)][string]$ResourceGroupName,
-    [Parameter(Mandatory)][string]$AppInsightsName,
-    $location = "East US"
-  )
-  # crear app insights
-
-  New-AzApplicationInsights  `
-  -Name $AppInsightsName `
-  -ResourceGroupName $ResourceGroupName `
-  -Location $location `
 
 
-  #validar que se haya creado
-  if (ValidateResourceExists -RsgOrRsc "rsc" -ResourceName $AppInsightsName) {
-    Write-LogCustom -Message "AppInsight $AppInsightsName created successfully"
-  }
-  else {
-    Write-LogCustom -Message "Failed to create AppInsight $AppInsightsName"
-  }
-}
+# function CreateResourceGroup {
+#   param(
+#     [Parameter(Mandatory)][string]$ResourceGroupName,
+#     $location = "eastus"
+#   )
+#   if (!(ValidateResourceExists -RsgOrRsc "rsg" -ResourceName $ResourceGroupName)) {
+#     New-AzResourceGroup -Name $ResourceGroupName -Location $location
+#   }
+# }
 
-function DeleteResource {
-  # Delete all the resources
-  param(
-    [Parameter(Mandatory = $null)][string]$ResourceGroupName,
-    [Parameter(Mandatory = $null)][string]$AppServicePlanName,
-    [Parameter(Mandatory = $null)][string]$AppServiceName
+# function CreateAppServicePlan {
+#   param(
+#     [Parameter(Mandatory)][string]$ResourceGroupName,
+#     [Parameter(Mandatory)][string]$AppServicePlanName,
+#     $location = "East US"
+#   )
+#   # crear asp
+#   New-AzAppServicePlan -ResourceGroupName $ResourceGroupName -Name $AppServicePlanName -Location $location -Tier "F1"
+#   #validar que se haya creado
+#   if (ValidateResourceExists -RsgOrRsc "rsc" -ResourceName $AppServicePlanName) {
+#     Write-LogCustom -Message "App Service Plan $AppServicePlanName created successfully"
+#   }
+#   else {
+#     Write-LogCustom -Message "Failed to create App Service Plan $AppServicePlanName"
+#   }
+# }
 
-  )
-  if ($ResourceGroupName) {
-    $existingResourceGroup = Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -eq $ResourceGroupName }
-    if ($null -eq $existingResourceGroup) {
-      Write-LogCustom -Message "There is no Resource Group named $ResourceGroupName"
-    }
-    else {
-      Write-LogCustom -Message "Starting deleted of Resource Group named $ResourceGroupName.."
-      $r = Remove-AzResourceGroup -Name $ResourceGroupName -Force
-      Start-sleep -Seconds 10
-      $existingResourceGroup = Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -eq $ResourceGroupName }
-      if (!$existingResourceGroup) {
-        Write-LogCustom -Message "The Resource Group $ResourceGroupName was deleted successfully "
-      }
-    }
-  }
-  if ($AppServicePlanName) {
-    $allresource = Get-AzResource | Where-Object { $_.Name -eq $AppServicePlanName } | Select-Object ResourceGroupName
-    if ($null -eq $allresource) {
-      Write-LogCustom -Message "There is no App Service Plan named $AppServicePlanName"
-    }
-    else {
-      $listresource = @()
-      foreach ($resource in $allresource) {
-        $listresource += $resource.ResourceGroupName
-      }
-      if ($listresource.Count -ge 2) {
-        Write-LogCustom -Message "There are more than one resource with the name $AppServicePlanName. Please, write the ResourceGroupName to continue with the deleted."
-        $RGN = Read-Host("Resourse Group Name")
-        Remove-AzResource `
-          -ResourceGroupName $RGN `
-          -ResourceName $AppServicePlanName `
-          -ResourceType Microsoft.Web/serverfarms `
-          -Force
-      }
-      else {
-        Write-LogCustom -Message "Starting deleted of App Service Plan named $AppServicePlanName.."
-        $r = Remove-AzResource `
-          -ResourceGroupName $listresource[0] `
-          -ResourceName $AppServicePlanName `
-          -ResourceType Microsoft.Web/serverfarms `
-          -Force
-        Start-sleep -Seconds 10
-        # $existingResource = Get-AzResource | Where-Object { $_.Name -eq $AppServicePlanName }
-        if ($r -eq $true) {
-          Write-LogCustom -Message "The App Service Plan $AppServicePlanName was deleted successfully"
-        }
-        else {
-          Write-LogCustom -Message "Failed to delete App Service Plan named $AppServicePlanName"
-        }
-      }
-    }
-  }
+# function CreateAppService {
+#   param(
+#     [Parameter(Mandatory)][string]$ResourceGroupName,
+#     [Parameter(Mandatory)][string]$AppServicePlanName,
+#     [Parameter(Mandatory)][string]$AppServiceName,
+#     $location = "West US"
+#   )
+#   # create app service WAP
 
-}
+#   New-AzWebApp  `
+#     -Name $AppServiceName `
+#     -ResourceGroupName $ResourceGroupName `
+#     -AppServicePlan $AppServicePlanName   `
+#     -Location $location `
+
+#   #Validate if resource exist
+#   if (ValidateResourceExists -RsgOrRsc "rsc" -ResourceName $AppServiceName) {
+#     Write-LogCustom -Message "App Service $AppServiceName created successfully"
+#   }
+#   else {
+#     Write-LogCustom -Message "Failed to create App Service Plan $AppServiceName"
+#   }
+# }
+
+# function CreateAppInsights {
+#   param(
+#     [Parameter(Mandatory)][string]$ResourceGroupName,
+#     [Parameter(Mandatory)][string]$AppInsightsName,
+#     $location = "East US"
+#   )
+#   # crear app insights
+
+#   New-AzApplicationInsights  `
+#   -Name $AppInsightsName `
+#   -ResourceGroupName $ResourceGroupName `
+#   -Location $location `
+
+
+#   #validar que se haya creado
+#   if (ValidateResourceExists -RsgOrRsc "rsc" -ResourceName $AppInsightsName) {
+#     Write-LogCustom -Message "AppInsight $AppInsightsName created successfully"
+#   }
+#   else {
+#     Write-LogCustom -Message "Failed to create AppInsight $AppInsightsName"
+#   }
+# }
+
+# function DeleteResource {
+#   # Delete all the resources
+#   param(
+#     [Parameter(Mandatory = $null)][string]$ResourceGroupName,
+#     [Parameter(Mandatory = $null)][string]$AppServicePlanName,
+#     [Parameter(Mandatory = $null)][string]$AppServiceName
+
+#   )
+#   if ($ResourceGroupName) {
+#     $existingResourceGroup = Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -eq $ResourceGroupName }
+#     if ($null -eq $existingResourceGroup) {
+#       Write-LogCustom -Message "There is no Resource Group named $ResourceGroupName"
+#     }
+#     else {
+#       Write-LogCustom -Message "Starting deleted of Resource Group named $ResourceGroupName.."
+#       $r = Remove-AzResourceGroup -Name $ResourceGroupName -Force
+#       Start-sleep -Seconds 10
+#       $existingResourceGroup = Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -eq $ResourceGroupName }
+#       if (!$existingResourceGroup) {
+#         Write-LogCustom -Message "The Resource Group $ResourceGroupName was deleted successfully "
+#       }
+#     }
+#   }
+#   if ($AppServicePlanName) {
+#     $allresource = Get-AzResource | Where-Object { $_.Name -eq $AppServicePlanName } | Select-Object ResourceGroupName
+#     if ($null -eq $allresource) {
+#       Write-LogCustom -Message "There is no App Service Plan named $AppServicePlanName"
+#     }
+#     else {
+#       $listresource = @()
+#       foreach ($resource in $allresource) {
+#         $listresource += $resource.ResourceGroupName
+#       }
+#       if ($listresource.Count -ge 2) {
+#         Write-LogCustom -Message "There are more than one resource with the name $AppServicePlanName. Please, write the ResourceGroupName to continue with the deleted."
+#         $RGN = Read-Host("Resourse Group Name")
+#         Remove-AzResource `
+#           -ResourceGroupName $RGN `
+#           -ResourceName $AppServicePlanName `
+#           -ResourceType Microsoft.Web/serverfarms `
+#           -Force
+#       }
+#       else {
+#         Write-LogCustom -Message "Starting deleted of App Service Plan named $AppServicePlanName.."
+#         $r = Remove-AzResource `
+#           -ResourceGroupName $listresource[0] `
+#           -ResourceName $AppServicePlanName `
+#           -ResourceType Microsoft.Web/serverfarms `
+#           -Force
+#         Start-sleep -Seconds 10
+#         # $existingResource = Get-AzResource | Where-Object { $_.Name -eq $AppServicePlanName }
+#         if ($r -eq $true) {
+#           Write-LogCustom -Message "The App Service Plan $AppServicePlanName was deleted successfully"
+#         }
+#         else {
+#           Write-LogCustom -Message "Failed to delete App Service Plan named $AppServicePlanName"
+#         }
+#       }
+#     }
+#   }
+
+# }
 
 
 #------------------------------------------------------- 
